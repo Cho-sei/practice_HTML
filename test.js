b3c3c670e24cff18c52d224cae89a90087eb1966
Chosei <knct0420@gmail.com>
2018-07-17 21:34:42 +0900

    add javascript

---
+++ test.js
@@ -0,0 +1,9 @@
+document.getElementById("top").onclick = function(){
+	alert("こんにちわ")
+}
+document.getElementById("top").onmouseover = function(){
+	document.getElementById("top").style.color = "blue";
+}
+document.getElementById("top").onmouseout = function(){
+	document.getElementById("top").style.color = "black";
+}
