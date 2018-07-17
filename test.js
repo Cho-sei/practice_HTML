2ef50aa28340f530487b76c2373909a80c22bf41
Chosei <knct0420@gmail.com>
2018-07-17 22:35:56 +0900

    add onclick event

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-function menu_initialize(id){
-	document.getElementById(id).style.color = "black";
-	document.getElementById(id).style.textDecoration = "none";
-}
-
-function changecousor(id){
-	document.getElementById(id).style.color = "blue";
-	document.getElementById(id).style.textDecoration = "underline";
+function click(){
+	alert("clicked!");
 }
